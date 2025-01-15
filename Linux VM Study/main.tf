64280f7a5e3832c00c3d385422595d8a9b27317e
pmacoy <pauloa.julio@gmail.com>
2025-01-15 22:56:39 +0000

    atualiazação subnet

--- Linux VM Study/main.tf
+++ Linux VM Study/main.tf
@@ -25,6 +25,13 @@ resource "azurerm_subnet" "subnet" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-study2"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
 resource "random_integer" "suffix" {
   min = 100000
   max = 999999
